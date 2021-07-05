b4882c818a4bfce891e47950a17a25862c64877b
itschandrakanth <chandrakanth@socialprachar.com>
2021-07-05 22:23:07 +0530

    revert GA

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,18 +11,13 @@ class CustomDocument extends Document {
       <Html lang="en-US">
         <Head>
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/0.0.0-359252c/tailwind.min.css" crossOrigin="anonymous" />
-            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-159451872-1"></script>
-            <script>
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'UA-159451872-1');
-            </script>
+            
         </Head>  
         <body>
           <Main />
           <NextScript />
           <script src="https://cloud.vajra.ai/vajra-support.js"></script>
+          
         </body>
       </Html>
     );
