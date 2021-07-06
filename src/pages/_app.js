07c639edf8be9ecce5d973642f05cd8f99872afa
itschandrakanth <chandrakanth@socialprachar.com>
2021-07-06 09:59:06 +0530

    working GA

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,28 @@
 import { useEffect } from 'react';
-import Router from 'next/router';
-import { initGA, logPageView } from 'analytics';
+import { useRouter } from 'next/router'
 import 'components/modal/modal.css';
 import 'typeface-dm-sans';
 
+import * as ga from '../lib/ga'
+
 export default function CustomApp({ Component, pageProps }) {
+
+  const router = useRouter()
+
   useEffect(() => {
-    initGA();
-    logPageView();
-    Router.events.on('routeChangeComplete', logPageView);
-  }, []);
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
 
   return <Component {...pageProps} />;
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -17,7 +17,22 @@ class CustomDocument extends Document {
           <Main />
           <NextScript />
           <script src="https://cloud.vajra.ai/vajra-support.js"></script>
-          
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </body>
       </Html>
     );
