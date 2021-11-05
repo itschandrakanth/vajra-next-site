5c7cb97ffa2831444888d09d4615f610ccc9df98
itschandrakanth <chandrakanth@socialprachar.com>
2021-11-05 16:16:12 +0530

    async await mail update 2

--- src/sections/contact.js
+++ src/sections/contact.js
@@ -28,7 +28,7 @@ const Contact = () => {
       
         
       
-        await fetch('/api/mail', {
+        await fetch('../api/mail', {
           method: 'POST',
           body: JSON.stringify(formData)
         }).then((res) => {
