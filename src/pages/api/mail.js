9d1d98d4f104a0a2c202af58ceb37ff5a9cff202
itschandrakanth <chandrakanth@socialprachar.com>
2021-11-08 15:45:39 +0530

    testing build command

--- src/pages/api/mail.js
+++ src/pages/api/mail.js
@@ -51,38 +51,67 @@
 // };
 
 // try - 3
+// const mail = require('@sendgrid/mail');
+
+// mail.setApiKey(process.env.SENDGRID_API_KEY);
+
+
+
+
+
+// export default async (req, res) => {
+//   const body = JSON.parse(req.body);
+
+//   const message = `
+//   Name: ${body.name}\r\n
+//   Email: ${body.email}\r\n
+//   Phone: ${body.phone}\r\n
+//   Website Url: ${body.homepage}\r\n
+//   Used Bot Before: ${body.bot_before}\r\n
+// `;
+
+//   console.log('body', body);
+//   try {
+//     await mail.send({
+//       to: 'chandrakanth@socialprachar.com',
+//       from: 'no-reply@vajra.ai',
+//       subject: 'New Response from Vajra Site!',
+//       text: message,
+//       html: message.replace(/\r\n/g, '<br>'),
+//     })
+//   }
+//   catch (error) {
+//     return res.status(error.statusCode || 500).json({ error: error.message });
+//   }
+//   return res.status(200).json({ error: '' });
+
+// }
+
+
+// try - 4
+
 const mail = require('@sendgrid/mail');
 
 mail.setApiKey(process.env.SENDGRID_API_KEY);
-
-
-
-
 
 export default async (req, res) => {
   const body = JSON.parse(req.body);
 
   const message = `
-  Name: ${body.name}\r\n
-  Email: ${body.email}\r\n
-  Phone: ${body.phone}\r\n
-  Website Url: ${body.homepage}\r\n
-  Used Bot Before: ${body.bot_before}\r\n
-`;
+    Name: ${body.name}\r\n
+    Email: ${body.email}\r\n
+    Phone: ${body.phone}\r\n
+    Website Url: ${body.homepage}\r\n
+    Used Bot Before: ${body.bot_before}\r\n
+  `;
 
-  console.log('body', body);
-  try {
-    await mail.send({
-      to: 'chandrakanth@socialprachar.com',
-      from: 'no-reply@vajra.ai',
-      subject: 'New Response from Vajra Site!',
-      text: message,
-      html: message.replace(/\r\n/g, '<br>'),
-    })
-  }
-  catch (error) {
-    return res.status(error.statusCode || 500).json({ error: error.message });
-  }
-  return res.status(200).json({ error: '' });
-
+  await mail.send({
+    to: 'chandrakanth@socialprachar.com',
+    from: 'no-reply@vajra.ai',
+    subject: 'New Response from Vajra Site!',
+    text: message,
+    html: message.replace(/\r\n/g, '<br>'),
+  })
+  res.status(200).json({ status: 'Ok' });
 }
+
