8ce8c60e277636fd6a6c17ae1ca1c18f83d9c411
itschandrakanth <apple@apples-MacBook-Pro.local>
2022-04-15 11:34:27 +0530

    hotjar code

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -50,6 +50,22 @@ class CustomDocument extends Document {
             `,
               }}
             />
+          {/* Hotjar Heatmaps */}
+          <script
+            dangerouslySetInnerHTML={{
+            __html: `
+            (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:2926424,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=')
+            `,
+              }}
+          />
+
             {/* <noscript><img height="1" width="1" style="display:none"
             src="https://www.facebook.com/tr?id=264700875575907&ev=PageView&noscript=1"
             /></noscript> */}
