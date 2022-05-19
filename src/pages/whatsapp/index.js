b3c98867fe606932739524893c390242d5da8e66
itschandrakanth <chandrakanth@socialprachar.com>
2022-05-19 15:39:08 +0530

    whatsapp page

--- src/pages/whatsapp/index.js
+++ src/pages/whatsapp/index.js
@@ -33,6 +33,11 @@ import WaBgContainer from '../../assets/images/Icon-Grid.svg'
 import $ from 'jquery'
 // import WAChatVideo from '../../assets/animations/wa-chat-vajra2.mp4'
 import WAChatGif from '../../assets/animations/WAChat.gif'
+import Link from "next/link";
+import educationImage from "../../assets/images/icons/education.png";
+import healthcareImage from "../../assets/images/icons/healthcare.png";
+import realestateImage from "../../assets/images/icons/realestate.png";
+import ecommerceImage from "../../assets/images/icons/ecommerce.png";
 
 
 const navigation = [
@@ -524,6 +529,96 @@ export default function Example() {
                 </div>
               </div>
             </section>
+            <section class="text-gray-600 body-font">
+          <div class="container px-5 mx-auto mb-10">
+            <div class="flex flex-wrap w-full mb-20 flex-col items-center text-center">
+              <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">
+                Vajra Whatsapp - Business Use Cases
+              </h1>
+              <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">
+                How Businesses can use vajra.ai Whatsapp Business Automation
+                Services
+              </p>
+            </div>
+            <div class="flex flex-wrap -m-4">
+              <div class="xl:w-1/3 md:w-1/2 p-4 cursor-pointer hover:scale-110">
+                <div class="bg-blue-50 rounded-xl">
+                  <Link href="/use-case/education">
+                    <div class="p-6">
+                      <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+                        <img src={educationImage} alt="vajra" />
+                      </div>
+                      <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
+                        Education
+                      </h2>
+                      <p class="leading-relaxed text-base">
+                        Use WhatsApp to manage your admissions, monitor your
+                        students, and interact with them.
+                      </p>
+                      <button class="flex text-black bg-gray-50 border-0 my-5 py-2 px-4 focus:outline-none hover:bg-blue-100 rounded-full text-sm">Details →</button>
+                    </div>
+                  </Link>
+                  
+                </div>
+              </div>
+              <div class="xl:w-1/3 md:w-1/2 p-4 cursor-pointer hover:scale-110">
+                <div class="bg-red-50 rounded-xl">
+                  <Link href="/use-case/healthcare">
+                    <div class="p-6">
+                      <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+                        <img src={healthcareImage} alt="vajra" />
+                      </div>
+                      <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
+                        Health Care
+                      </h2>
+                      <p class="leading-relaxed text-base">
+                      We bring doctors closer to the patients through WhatsApp. 
+                      </p>
+                      <button class="flex text-black bg-gray-50 border-0 my-5 py-2 px-4 focus:outline-none hover:bg-red-100 rounded-full text-sm">Details →</button>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+              <div class="xl:w-1/3 md:w-1/2 p-4 cursor-pointer hover:scale-110">
+                <div class="bg-purple-50 rounded-xl">
+                  <Link href="/use-case/realestate">
+                    <div class="p-6">
+                      <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+                        <img src={realestateImage} alt="vajra" />
+                      </div>
+                      <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
+                        Real Estate
+                      </h2>
+                      <p class="leading-relaxed text-base">
+                      Harness the magic of WhatsApp to sell more properties with no stress!
+                      </p>
+                      <button class="flex text-black bg-gray-50 border-0 my-5 py-2 px-4 focus:outline-none hover:bg-purple-100 rounded-full text-sm">Details →</button>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+              <div class="xl:w-1/3 md:w-1/2 p-4 cursor-pointer hover:scale-110">
+                <div class="bg-pink-50 rounded-xl">
+                  <Link href="/use-case/ecommerce">
+                    <div class="p-6">
+                      <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+                        <img src={ecommerceImage} alt="vajra" />
+                      </div>
+                      <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
+                        E-Commerce
+                      </h2>
+                      <p class="leading-relaxed text-base">
+                      Increase your revenues and provide the best customer experience through WhatsApp 
+                      </p>
+                      <button class="flex text-black bg-gray-50 border-0 my-5 py-2 px-4 focus:outline-none hover:bg-pink-100 rounded-full text-sm">Details →</button>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            </div>
+            
+          </div>
+        </section>
 
             {/* Form */}
             <WhatsappForm />
